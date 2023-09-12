bd60c2f6545f0a402f4cdc7e3d17a0086a88b01c
ibrahim75 <ibrahimbiplob75@gmail.com>
2023-09-13 01:25:54 +0600

    show on view click mising

--- index.js
+++ index.js
@@ -1,80 +1,82 @@
-const loadCategory = async(id) =>{
-   
-    
-    const url = `https://openapi.programming-hero.com/api/videos/categories`
-    const res = await fetch(url);
-    const data = await res.json();
-    const category=data.data;
-    console.log(category)
-    showCategory(category)
-    
-    
-}
+// Define an empty array to store view counts
+const numbers = [];
+let data;
 
-function showCategory(data){
-    
-        const Category_list=document.getElementById('category_btn')
-        data.forEach(function(item) {
-            
-            const JS_div=document.createElement('div')
-            JS_div.classList=`p-6`;
+const loadCategory = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/videos/categories`;
+  const res = await fetch(url);
+  const response = await res.json();
+  data = response.data; 
+  showCategory(data);
+  
 
-        JS_div.innerHTML=`
+};
+
+
+function showCategory(data) {
+  const Category_list = document.getElementById('category_btn');
+  data.forEach(function (item) {
+    const JS_div = document.createElement('div');
+    JS_div.classList = `p-3`;
+
+    JS_div.innerHTML = `
                 <div class="card-actions justify-start mt-4 mb-3 ">
                 
                     <button id="category_id"  onclick="category_id('${item.category_id}')" class="btn bbtn-outline w-32">${item.category}</button>
                 </div>
-                
         
-        ` ;
-        
-       
-        Category_list.appendChild(JS_div)
-            
-        });
-         
-            
-}
-loadCategory()
+        `;
 
-//category Id on click from button
+    Category_list.appendChild(JS_div);
+  });
+
+  loadVideos(); 
+}
+
+// Category Id on click from button
 function category_id(categoryId) {
-    
-    loadVideos(categoryId)
-    
+  loadVideos(categoryId);
+}
+
+const loadVideos = async (Id) => {
+  const url = `https://openapi.programming-hero.com/api/videos/category/${Id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  const videos = data.data;
+ 
+
+  
+  numbers.length = 0; 
+  videos.forEach((element) => {
+    const array = element.others.views;
+    numbers.push(array);
+  });
+
+  display_vides(videos);
+ 
 }
 
 
 
-const loadVideos = async(Id) =>{
-    
-    const url = `https://openapi.programming-hero.com/api/videos/category/${Id}`
-    const res = await fetch(url);
-    const data = await res.json();
-    const videos=data.data;
-    console.log(videos)
-    display_vides(videos)
-    
-}
 
-function display_vides(data){
-    
-    let Length2=data.length;
-    console.log(Length2)
+function display_vides(data) {
+  let Length2 = data.length;
+  
 
+  if (Length2 > 0) {
+    var videoContainer = document.getElementById('video-container');
+    videoContainer.innerText = '';
 
-    if(Length2>0){
-        var videoContainer=document.getElementById('video-container');
-        videoContainer.innerText=''
-        
+    // Sort the videos based on view counts
+    const sortedVideos = data.sort(
+      (a, b) => parseFloat(b.others.views) - parseFloat(a.others.views)
+    );
 
-        data.forEach(element => {
-            
+    sortedVideos.forEach((element) => {
+      const JS_div = document.createElement('div');
+      JS_div.classList = `w-84 bg-base-100 p-2 mt-6 mb-4 rounded-sm`;
 
-            const JS_div=document.createElement('div')
-            JS_div.classList=`w-84 bg-base-100 p-2 mt-6 mb-4 rounded-sm`;
-
-            JS_div.innerHTML=`
+      JS_div.innerHTML = `
             <figure><img class="h-48 w-full rounded-lg" src="${element.thumbnail}" alt="Shoes" /></figure>
                 <div class="card-body">
                     <div class="flex flex-row">
@@ -84,7 +86,11 @@ function display_vides(data){
                     <div class="flex flex-row justify-start">
                         <p>${element.authors[0].profile_name}</p>
                         
-                        ${element.authors[0].verified ? '<img class="w-6" src="image/verified.png"></img>' : ''}
+                        ${
+                          element.authors[0].verified
+                            ? '<img class="w-6" src="image/verified.png"></img>'
+                            : ''
+                        }
                         
                         
                     </div>
@@ -93,24 +99,18 @@ function display_vides(data){
                     </div>
                 </div>
             
-            ` ;
-            
-            videoContainer.appendChild(JS_div)
-            
-        });
+            `;
 
-    }
-    else{
-        var videoContainer=document.getElementById('video-container');
-        videoContainer.innerText=''
+      videoContainer.appendChild(JS_div);
+    });
+  } else {
+    var videoContainer = document.getElementById('video-container');
+    videoContainer.innerText = '';
 
-        
-        
-        
-        const JS_div=document.createElement('div')
-            JS_div.classList=`rounded-sm`;
+    const JS_div = document.createElement('div');
+    JS_div.classList = `rounded-sm`;
 
-            JS_div.innerHTML=`
+    JS_div.innerHTML = `
                 <div class="flex justify-center items-center text-center mt-20">
                     <figure class="text-center">
                         <img class="rounded-lg mt-auto" src="image/Icon.png" alt="Shoes" />
@@ -119,26 +119,27 @@ function display_vides(data){
                 </div>
                  <h2 class="text-4xl text-center mt-6">Oops!! Sorry, There is no content here</h2>
                 
-            ` ;
-            
-            videoContainer.appendChild(JS_div)
-    }
-    
-    loader(false)
+            `;
+
+    videoContainer.appendChild(JS_div);
+  }
+
+  loader(false);
 }
 
-loadVideos()
 
-//Loqading functional implement
-const loader=(isLoading) =>{
-    const loaderId=document.getElementById('loading-spinner')
-        if(isLoading){
-            loaderId.classList.remove("hidden");
-        }
-        else{
-            
-            loaderId.classList.add("hidden");
-        }
-        
-}
 
+
+
+// Loading functional implementation
+const loader = (isLoading) => {
+  const loaderId = document.getElementById('loading-spinner');
+  if (isLoading) {
+    loaderId.classList.remove('hidden');
+  } else {
+    loaderId.classList.add('hidden');
+  }
+};
+
+
+loadCategory();
